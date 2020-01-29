2fac83b62c532de4eb14d062978381c6cdb6ae82
github username <github email address>
2020-01-29 09:19:08 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
   // your code here
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+
+
+  newTaskForm.addEventListener("submit", createNewTask);
+
+
+
+
+});
+
+const createNewTask = event => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const newTaskDescription = document.getElementById("new-task-description");
+  const listItems = document.createElement("li");
+  listItems.innerText = newTaskDescription.value;
+
+  appendNewTask(listItems);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  const tasksUl = document.getElementById("tasks");
+  tasksUl.appendChild(task);
+};
+
+const tasksUl = document.getElementById("tasks");
+
+
+tasksUl.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    taskList.deleteTask(e.target.dataset.description);
+    renderApp();
+  }
 });
